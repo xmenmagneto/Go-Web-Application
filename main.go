dc70bb3570b5967bcd501f9e29913974448d77ec
DengTingchang <dengtingchang@DengTingchangs-MacBook-Pro.local>
2017-11-23 16:48:38 -0600

    setup environment

---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"encoding/json"
+	"log"
+	"strconv"
+)
+const (
+	DISTANCE = "200km"
+)
+
+//type 等于java 中的class，也等于c++中的struc
+type Location struct {
+	Lat float64 `json:"lat"`
+	Lon float64 `json:"lon"`
+}
+
+type Post struct {
+	// `json:"user"` is for the json parsing of this User field. Otherwise, by default it's 'User'.
+	User     string `json:"user"`
+	Message  string  `json:"message"`
+	Location Location `json:"location"`
+}
+
+
+func main() {
+	fmt.Println("started-service")
+	http.HandleFunc("/post", handlerPost)
+	http.HandleFunc("/search", handlerSearch)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handlerPost(w http.ResponseWriter, r *http.Request) {
+	// Parse from body of request to get a json object.
+	fmt.Println("Received one post request")
+	decoder := json.NewDecoder(r.Body)
+	var p Post   //Post是之前定义的
+	if err := decoder.Decode(&p); err != nil {
+		panic(err)  //报错
+		return
+	}
+
+	fmt.Fprintf(w, "Post received: %s\n", p.Message)  //w是输出
+}
+
+func handlerSearch(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received one request for search")
+	lat, _ := strconv.ParseFloat(r.URL.Query().Get("lat"), 64)     //use string conversion library
+	lon, _ := strconv.ParseFloat(r.URL.Query().Get("lon"), 64)
+
+	// range is optional
+	ran := DISTANCE
+	if val := r.URL.Query().Get("range"); val != "" {
+		ran = val + "km"   //更新成用户给的distance
+
+	}
+	fmt.Fprintf(w, "Search received: %f %f %s", lat, lon, ran)
+
+	// Return a fake post
+	p := &Post{
+		User:"1111",
+		Message:"一生必去的100个地方",
+		Location: Location{
+			Lat:lat,
+			Lon:lon,
+		},
+	}
+
+	js, err := json.Marshal(p)   //把object序列化
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+
+
+
