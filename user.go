aab8bab5d5b3c3dd99ddb7a6700d4a9fa58049b0
DengTingchang <dengtingchang@DengTingchangs-MacBook-Pro.local>
2017-12-15 10:17:47 -0600

    checkUser and addUser

--- user.go
+++ user.go
@@ -20,4 +20,84 @@ type User struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
-// checkUser checks whether user is valid
+
+// checkUser checks whether credential is valid while login
+func checkUser(username, password string) bool {
+	es_client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		fmt.Printf("ES is not setup %v\n", err)
+		return false
+	}
+
+	// Search with a term query
+	termQuery := elastic.NewTermQuery("username", username)
+	queryResult, err := es_client.Search().
+		Index(INDEX).
+		Query(termQuery).
+		Pretty(true).
+		Do()
+	if err != nil {
+		fmt.Printf("ES query failed %v\n", err)
+		return false
+	}
+
+	var tyu User
+	for _, item := range queryResult.Each(reflect.TypeOf(tyu)) {
+		u := item.(User) //强制转换为User
+		return u.Password == password && u.Username == username
+	}
+	// If no user exist, return false.
+	return false
+}
+
+
+// Add a new user. Return true if successfully.
+func addUser(username, password string) bool {
+	// In theory, BigTable is a better option for storing user credentials than ES. However,
+	// since BT is more expensive than ES so usually students will disable BT.
+	es_client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		fmt.Printf("ES is not setup %v\n", err)
+		return false
+	}
+
+	user := &User{
+		Username: username,
+		Password: password,
+	}
+
+	// Search with a term query
+	termQuery := elastic.NewTermQuery("username", username)
+	queryResult, err := es_client.Search().
+		Index(INDEX).
+		Query(termQuery).
+		Pretty(true).
+		Do()
+	if err != nil {
+		fmt.Printf("ES query failed %v\n", err)
+		return false
+	}
+	if queryResult.TotalHits() > 0 {
+		fmt.Printf("User %s has existed, cannot create duplicate user.\n", username)
+		return false
+	}
+
+	// Save it to index
+	_, err = es_client.Index().
+		Index(INDEX).
+		Type(TYPE_USER).
+		Id(username).
+		BodyJson(user).
+		Refresh(true).
+		Do()
+	if err != nil {
+		fmt.Printf("ES save failed %v\n", err)
+		return false
+	}
+	return true
+}
+
+// If signup is successful, a new session is created.
+
+
+
