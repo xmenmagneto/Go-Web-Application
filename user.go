2a6b2d5be418d14141f875ca5344c8d4158e47f6
DengTingchang <dengtingchang@DengTingchangs-MacBook-Pro.local>
2017-12-15 10:31:28 -0600

    add loginHandler

--- user.go
+++ user.go
@@ -10,6 +10,7 @@ import (
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
+	"strconv"
 )
 
 const (
@@ -98,6 +99,63 @@ func addUser(username, password string) bool {
 }
 
 // If signup is successful, a new session is created.
+func signupHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received one signup request")
+
+	decoder := json.NewDecoder(r.Body)
+	var u User
+	if err := decoder.Decode(&u); err != nil {
+		panic(err)
+		return
+	}
+
+	if u.Username != "" && u.Password != "" {
+		if addUser(u.Username, u.Password) {
+			fmt.Println("User added successfully.")
+			w.Write([]byte("User added successfully."))
+		} else {
+			fmt.Println("Failed to add a new user.")
+			http.Error(w, "Failed to add a new user", http.StatusInternalServerError) //500
+		}
+	} else {
+		fmt.Println("Empty password or username.")
+		http.Error(w, "Empty password or username", http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+}
+
+// If login is successful, a new token is created.
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received one login request")
+
+	decoder := json.NewDecoder(r.Body)
+	var u User
+	if err := decoder.Decode(&u); err != nil {
+		panic(err)
+		return
+	}
+
+	if checkUser(u.Username, u.Password) {
+		token := jwt.New(jwt.SigningMethodHS256)
+		claims := token.Claims.(jwt.MapClaims)
+		/* Set token claims */
+		claims["username"] = u.Username
+		claims["exp"] = time.Now().Add(time.Hour * 24).Unix()  //expiration time
+
+		/* Sign the token with our secret */
+		tokenString, _ := token.SignedString(mySigningKey) //加密
+		/* Finally, write the token to the browser window */
+		w.Write([]byte(tokenString))
+	} else {
+		fmt.Println("Invalid password or username.")
+		http.Error(w, "Invalid password or username", http.StatusForbidden)
+	}
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+}
 
 
 
